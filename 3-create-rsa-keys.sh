ab65871ba7fcfb1cc367a827477de0fcc04143fe
kerami <keramiozsoy@hotmail.com>
2023-07-28 20:55:00 +0300

    Update 3-create-rsa-keys.sh

--- 3-create-rsa-keys.sh
+++ 3-create-rsa-keys.sh
@@ -2,8 +2,20 @@
 
 cd $HOME
 ls -ltr ~/.ssh
-ssh-keygen || echo || echo || echo || echo
+
+ssh-keygen 
+
+# enter
+
+# enter 
+
+# enter
+
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
 ssh localhost
+
+# yes
+
 exit
 
